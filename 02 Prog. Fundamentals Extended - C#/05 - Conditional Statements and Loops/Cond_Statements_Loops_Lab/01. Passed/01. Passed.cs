7700e746f5ffb7d514fc046c1f6c4b2bc552cd6f
Nikola Grigorov <niko1986@abv.bg>
2018-01-30 18:26:20 +0200

    exercise

---
+++ 02 Prog. Fundamentals Extended - C#/05 - Conditional Statements and Loops/Cond_Statements_Loops_Lab/01. Passed/01. Passed.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Passed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+
+            bool result = (n >= 3.00);
+
+            if (result == true)
+            {
+                Console.WriteLine("Passed!");
+            }
+
+            if (n >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
