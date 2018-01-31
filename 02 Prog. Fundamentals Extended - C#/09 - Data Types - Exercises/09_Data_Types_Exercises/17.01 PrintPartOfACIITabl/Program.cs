541205c046a4ecd3b5d9fd1818440fa60c204cc4
Nikola Grigorov <niko1986@abv.bg>
2018-01-31 10:25:54 +0200

    exercise

---
+++ 02 Prog. Fundamentals Extended - C#/09 - Data Types - Exercises/09_Data_Types_Exercises/17.01 PrintPartOfACIITabl/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._01_PrintPartOfACIITabl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startNum = int.Parse(Console.ReadLine());
+            var finalNum = int.Parse(Console.ReadLine());
+
+            for (int i = startNum; i <= finalNum; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
