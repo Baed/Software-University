b6ffe04614239df29f6c7ab2eb8778848e59fdc2
Nikola Grigorov <niko1986@abv.bg>
2018-01-31 08:49:57 +0200

    exercise

---
+++ 02 Prog. Fundamentals Extended - C#/09 - Data Types - Exercises/09_Data_Types_Exercises/15. Calculator/15. Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            string operating = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+
+            switch (operating)
+            {
+                case "+": Console.WriteLine($"{a} + {b} = {a + b}"); break;
+                case "-": Console.WriteLine($"{a} - {b} = {a - b}"); break;
+                case "*": Console.WriteLine($"{a} * {b} = {a * b}"); break;
+                case "/": Console.WriteLine($"{a} / {b} = {a / b}"); break;
+            }
+        }
+    }
+}
---
+++ 02 Prog. Fundamentals Extended - C#/09 - Data Types - Exercises/09_Data_Types_Exercises/16. Tricky Strings/16. Tricky Strings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.Tricky_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string delimiter = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            var word = "";
+            for (int i = 0; i < n; i++)
+            {
+                string currentText = Console.ReadLine();
+                word += i < n - 1 ? currentText + delimiter : currentText;
+            }
+            Console.Write("{0}", word);
+            Console.WriteLine();
+        }
+    }
+}
