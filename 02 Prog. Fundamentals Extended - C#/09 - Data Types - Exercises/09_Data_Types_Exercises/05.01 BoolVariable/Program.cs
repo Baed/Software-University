bd8d44abd5ce416902cd900c176383ddf1b715a2
Nikola Grigorov <niko1986@abv.bg>
2018-01-31 10:25:54 +0200

    exercise

---
+++ 02 Prog. Fundamentals Extended - C#/09 - Data Types - Exercises/09_Data_Types_Exercises/05.01 BoolVariable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05._01_BoolVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (bool.Parse(input))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
