f6935ae8b7dc49621a49e8ac1ddbc84d9e96adb8
Nikola Grigorov <niko1986@abv.bg>
2018-02-02 08:39:57 +0200

    exercise

---
+++ 02 Prog. Fundamentals Extended - C#/05 - Conditional Statements and Loops/Cond_Statements_Loops_Lab/11. Odd Number/11. Odd Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Odd_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            while (true)
+            {
+                int n = Math.Abs(int.Parse(Console.ReadLine()));
+                if (n % 2 == 1)
+                {
+                    Console.WriteLine($"The number is: {n}"); // mojem da go premestim dolu
+                    break;
+                }
+                Console.WriteLine($"Please write an odd number.");
+            }
+           // Console.WriteLine($"The number is: {n}"); ako go izvadim ot if - construkciqta
+        }
+    }
+}
