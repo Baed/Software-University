9a8524e79071215da82385e40b95fde1a9553719
Nikola Grigorov <niko1986@abv.bg>
2018-02-03 18:20:42 +0200

    exercise

--- 05 OOP Advanced/10 Exam Preparations/07 August 2016/RecyclingStation_Skeleton/RecyclingStation/WasteDisposal/GarbageProcessor.cs
+++ 05 OOP Advanced/10 Exam Preparations/07 August 2016/RecyclingStation_Skeleton/RecyclingStation/WasteDisposal/GarbageProcessor.cs
@@ -7,13 +7,15 @@
 
     public class GarbageProcessor : IGarbageProcessor
     {
+        public GarbageProcessor() 
+            : this(new StrategyHolder())
+        {
+
+        }
+
         public GarbageProcessor(IStrategyHolder strategyHolder)
         {
             this.StrategyHolder = strategyHolder;
-        }
-
-        public GarbageProcessor() : this(new StrategyHolder())
-        {
         }
 
         public IStrategyHolder StrategyHolder { get; private set; }
@@ -21,9 +23,15 @@
         public IProcessingData ProcessWaste(IWaste garbage)
         {
             Type type = garbage.GetType();
-            DisposableAttribute disposalAttribute = (DisposableAttribute)type.GetCustomAttributes(true).FirstOrDefault(x => x.GetType() == typeof(DisposableAttribute));
+            DisposableAttribute disposalAttribute = (DisposableAttribute)type.
+                GetCustomAttributes(true)
+                .FirstOrDefault(x => x.GetType() == typeof(DisposableAttribute));
+
             IGarbageDisposalStrategy currentStrategy;
-            if (disposalAttribute == null || !this.StrategyHolder.GetDisposalStrategies.TryGetValue(disposalAttribute.GetType(), out currentStrategy))
+            if (disposalAttribute == null || 
+                !this.StrategyHolder
+                            .GetDisposalStrategies
+                            .TryGetValue(disposalAttribute.GetType(), out currentStrategy))
             {
                 throw new ArgumentException(
                     "The passed in garbage does not implement a supported Disposable Strategy Attribute.");
