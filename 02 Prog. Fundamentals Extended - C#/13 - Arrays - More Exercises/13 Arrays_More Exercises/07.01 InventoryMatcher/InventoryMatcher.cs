901c4da76c9ed17a0546056edf8bb271dbc5d52e
Nikola Grigorov <niko1986@abv.bg>
2018-02-08 14:29:53 +0200

    Programming Fundamentals January 2018

---
+++ 02 Prog. Fundamentals Extended - C#/13 - Arrays - More Exercises/13 Arrays_More Exercises/07.01 InventoryMatcher/InventoryMatcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._01_InventoryMatcher
+{
+    class InventoryMatcher
+    {
+        static void Main(string[] args)
+        {
+            string[] productsArr = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            long[] quantityArr = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+
+            decimal[] pricesArr = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(decimal.Parse).ToArray();
+
+            string command;
+            while ((command = Console.ReadLine()) != "done")
+            {
+                int ìndex = Array.IndexOf(productsArr, command);
+
+                Console.WriteLine($"{productsArr[ìndex]} costs: {pricesArr[ìndex]}; Available quantity: {quantityArr[ìndex]}");
+            }
+        }
+    }
+}
