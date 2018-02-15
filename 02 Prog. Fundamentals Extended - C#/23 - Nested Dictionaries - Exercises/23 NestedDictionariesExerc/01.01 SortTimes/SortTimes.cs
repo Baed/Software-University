25e9e6e99b7546f0681da283eeba98d394e28e8f
Nikola Grigorov <niko1986@abv.bg>
2018-02-15 17:30:07 +0200

    exercise

---
+++ 02 Prog. Fundamentals Extended - C#/23 - Nested Dictionaries - Exercises/23 NestedDictionariesExerc/01.01 SortTimes/SortTimes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._01_SortTimes
+{
+    class SortTimes
+    {
+        static void Main(string[] args)
+        {
+            var timeList = Console.ReadLine()
+                .Split(' ')
+                .OrderBy(t => t)
+                .ToList();
+            Console.WriteLine(string.Join(", ", timeList));
+        }
+    }
+}
